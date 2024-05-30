210180b170dda3562d9a397c6b15df3a2979ab5a
carion21 <medimickael99@gmail.com>
2024-05-30 17:29:27 +0000

    Initial version

---
+++ main.py
@@ -0,0 +1,35 @@
+# main.py
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+import requests
+import time
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/api/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def proxy(path):
+    url = f'{os.getenv("HOST")}/{path}'
+
+    start = time.time()
+
+    response = requests.request(
+        method=request.method,
+        url=url,
+        headers={key: value for key,
+                 value in request.headers if key != 'Host'},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
+
+    end = time.time()
+
+    print(f'{url} - {response.status_code} - {end-start} seconds')
+
+    return (response.content, response.status_code, response.headers.items())
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT'))
+    app.run(host='0.0.0.0', port=port, debug=True)
